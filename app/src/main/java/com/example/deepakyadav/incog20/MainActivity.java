3ce99a497a5fbfb4a205d3eb5dcfcfa3bee11dea
Deepak Yadav <deepakyadav23@github.com>
2018-04-09 10:58:47 +0530

    setrdyt

--- app/src/main/java/com/example/deepakyadav/incog20/MainActivity.java
+++ app/src/main/java/com/example/deepakyadav/incog20/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.deepakyadav.incog20;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,24 +13,23 @@ import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(null);
 
         WebView webView = findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("http://www.youtube.com");
 
+
     }
+
     public void openDialog(View view){
         Toast.makeText(this, "open dialog", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
